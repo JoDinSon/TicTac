574e600487fea42970faa525e3efeb40a5b640e7
Joey6406 <johannes.hoerl@web.de>
2024-02-05 07:33:02 +0100

    working on mouse interaction Added a field class representing each field on the board added a check_click listener (name is actually confusing) that takes mouse position and checks wether mouse hovers over field if so is draws an orange rectange on the inner border of the field if the mouse is not on the field it draws a white rectangle to remove the orange one.
    Also added an X and a Circle for experimenting

--- UI.py
+++ UI.py
@@ -7,6 +7,8 @@ pygame.init()
 # 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+ORANGE = (255, 140, 0)
+BLUE = (100, 149, 237)
 
 # Open window
 screen = pygame.display.set_mode((300, 300))
@@ -19,6 +21,23 @@ pygame.display.set_caption("TicTacToe")
 
 #set refresh
 clock = pygame.time.Clock()
+
+# create individual fields to geht mouse location
+class field():
+    def __init__(self):
+        self.rect = pygame.Rect(0, 0, 100, 100)
+
+    #highlight if mouse hovers over current field
+    def check_click(self, mouse):
+        if self.rect.collidepoint(mouse):
+            pygame.draw.rect(screen, ORANGE, [0, 0, 99, 99], 4)
+        else:
+            pygame.draw.rect(screen, WHITE, [0, 0, 99, 99], 4)
+
+
+
+field_1 = field()
+
 
 
 # Game loop
@@ -37,10 +56,23 @@ while active:
     pygame.draw.line(screen, BLACK, [100, 0], [100, 300], 4)
     pygame.draw.line(screen, BLACK, [200, 0], [200, 300], 4)
 
+    # draw an x
+    pygame.draw.line(screen, ORANGE, [15, 15], [85, 85], 6)
+    pygame.draw.line(screen, ORANGE, [85, 15], [15, 85], 6)
+
+    # draw circle
+    pygame.draw.ellipse(screen, BLUE, [215, 15, 70, 70], 6)
+
+   
+    field_1.check_click(pygame.mouse.get_pos())
+
+
+  
+    
+
+
     # display refresh
     pygame.display.flip()
-
-    
 
     #refresh time
     clock.tick(60)
