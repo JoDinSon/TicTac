a51e13f1e08792e71e4af621e43aba10fdfba301
Joey6406 <johannes.hoerl@web.de>
2024-02-05 23:20:42 +0100

    added fields, started implementing click listener Removed manually drawn lines on the game board. Instead created 9 instances of the field class and arranged them in a way that they form the game board. All done in a for loop. Then started working on adding functionality to the check_click function. The aim is to detect when the mouse hovers over a field and is clicked. However the click lsitener doesnt seem to work so far.

--- UI.py
+++ UI.py
@@ -23,20 +23,54 @@ pygame.display.set_caption("TicTacToe")
 clock = pygame.time.Clock()
 
 # create individual fields to geht mouse location
-class field():
-    def __init__(self):
-        self.rect = pygame.Rect(0, 0, 100, 100)
-
+class field:
+    def __init__(self, pos_x, pos_y, width, height):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.height = height
+        self.width = width
+        self.rect = pygame.Rect(self.pos_x, self.pos_y, self.width, self.height)
+        pygame.draw.rect(screen, BLACK, [self.pos_x, self.pos_y, self.width, self.height], 3)
     #highlight if mouse hovers over current field
     def check_click(self, mouse):
+        
         if self.rect.collidepoint(mouse):
-            pygame.draw.rect(screen, ORANGE, [0, 0, 99, 99], 4)
+            pygame.draw.rect(screen, ORANGE, [self.pos_x + 3, self.pos_y + 3, self.width - 4, self.height - 4], 2)
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    print("mouse was clicked")
+                    self.draw_x()
         else:
-            pygame.draw.rect(screen, WHITE, [0, 0, 99, 99], 4)
+            pygame.draw.rect(screen, WHITE, [self.pos_x + 3, self.pos_y + 3, self.width - 4, self.height - 4], 2)
+    
+        # draw an x
+    def draw_x(self):
+        pygame.draw.line(screen, ORANGE, [self.pos_x + 15, self.pos_y + 15], [self.pos_x + 85, self.pos_y + 85], 6)
+        pygame.draw.line(screen, ORANGE, [self.pos_x + 85, self.pos_y + 15], [self.pos_x + 15, self.pos_y + 85], 6)
 
 
 
-field_1 = field()
+
+# Create Names for fields
+num_fields = 9
+fields_names = [f'field_{i+1}' for i in range(num_fields)]
+
+#creating the empty field variables
+fields = [0 for _ in range(len(fields_names))]
+
+# set size of the individual rectangles
+field_side = 100
+
+# fill the list of fields with field instances
+index = 0
+for i in range(3):
+    for j in range(3):
+        fields[index] = field(i * field_side, j * field_side, field_side, field_side)
+        index += 1
+    
+
+
+#field_1 = field(100, 0, 100, 100)
 
 
 
@@ -49,22 +83,23 @@ while active:
             active = False
 
     # draw two horizontal lines
+    '''
     pygame.draw.line(screen, BLACK, [0, 100], [300, 100], 4)
     pygame.draw.line(screen, BLACK, [0, 200], [300, 200], 4)
 
     # draw two vertical lines
     pygame.draw.line(screen, BLACK, [100, 0], [100, 300], 4)
     pygame.draw.line(screen, BLACK, [200, 0], [200, 300], 4)
+    '''
 
-    # draw an x
-    pygame.draw.line(screen, ORANGE, [15, 15], [85, 85], 6)
-    pygame.draw.line(screen, ORANGE, [85, 15], [15, 85], 6)
+
+
 
     # draw circle
     pygame.draw.ellipse(screen, BLUE, [215, 15, 70, 70], 6)
 
-   
-    field_1.check_click(pygame.mouse.get_pos())
+    for i in range(len(fields)):
+        fields[i].check_click(pygame.mouse.get_pos())
 
 
   
